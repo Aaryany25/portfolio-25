7095a3eb45a67f4b03ec2173be976a0db1ebd096
Aaryan <aaryany2505@gmail.com>
2025-10-26 01:08:51 +0530

    Made Infinite Scroll Effect

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Basic Setup
+const container = document.querySelector('#container');
+const scroller = document.querySelector('#scroller');
+
+// 2. Calculate heights
+const sectionHeight = window.innerHeight;
+const totalSections = 3;
+const sequenceHeight = sectionHeight * totalSections;
+// console.log(window.innerHeight)
+// 3. Start from middle (2nd set)
+let currentY = sequenceHeight; //2805
+scroller.style.transform = `translateY(-${currentY}px)`;
+
+// 4. On scroll
+container.addEventListener('wheel', (e) => {
+    currentY += e.deltaY; // 5650
+    
+    // Jump check    //  5650>5610 -467
+    if (currentY > sequenceHeight * 2 - sectionHeight/2) {
+        currentY -= sequenceHeight; // Jump back
+        //currentY = CurrentY - 5610
+    } 
+    if (currentY < sectionHeight/2) {
+        currentY += sequenceHeight; // Jump forward
+    }
+    
+    scroller.style.transform = `translateY(-${currentY}px)`;
+});
