7ca1d0b77a475411c92cc2c2fe99b448abcfc8a0
Aaryan <aaryany2505@gmail.com>
2025-11-02 17:24:44 +0530

    Added cursur Animation

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+function locomotiveAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
 });
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function InfinteScroll(){
 // 1. Basic Setup
 
@@ -33,7 +62,15 @@ container.addEventListener('wheel', (e) => {
     scroller.style.transform = `translateY(-${currentY}px)`;
 });
 }
+function CursurAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
 
+}
 function Background(){
 const krishna = document.querySelector('.krishna');
 const galaxy = document.querySelector('.galaxy');
@@ -62,7 +99,41 @@ document.addEventListener('mouseleave', () => {
 });
 
 }
+
+// For Now Iam using this function later I will replace it with GSAP TextSplit 
+// function textsplitting(){
+//     var H1=document.querySelectorAll(".textSplit")
+//     H1.forEach(element => {
+//         var h1text = element.textContent
+//         var clutter=""
+//         var splitted = h1text.split(" ")
+//         splitted.forEach(function(elem){
+//             clutter +=`<span>${elem}</span>`
+           
+//         })
+//         element.innerHTML = clutter
+//     });
+// }
+// function gsapAnimation(){
+//     gsap.to(".textSplit",{
+//         color:"#e3e3c4",
+//         stagger:0.1,
+//         scrollTrigger:{
+//             trigger:".textSplit",
+//             scroller:"#main",
+//            markers:true,
+//             start:"top 40%",
+//             end:"top -20%",
+//             scrub:5,
+            
+//         }
+    
+//     })
+// }
+
+locomotiveAnimation()
 InfinteScroll() 
 Background()
-
-
+CursurAnimation()
+// textsplitting()
+// gsapAnimation()
